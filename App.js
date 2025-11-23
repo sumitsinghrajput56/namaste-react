5dd0daca80286feba2a6199978daacf820f10a6d
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2025-11-24 01:42:05 +0530

    worked with caching and react and react dom install or read about dist file

--- App.js
+++ App.js
@@ -1,3 +1,9 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+
+
+
+
 const heading = React.createElement("div", {id: "parent"},[React.createElement("div", {id: "child"},[
     React.createElement("h1", {id: "heading"},"first"),
     React.createElement("h1", {id: "heading"},"second"),
