b04a190fec402768cb931df8caabc52ba8a64c90
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2025-11-25 20:46:07 +0530

    write code in jsx and react which is like html

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from "react";
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 
-// changing Object -> HTMLElemet using render method 
+// React.createElement => objcet => HTMLElement(render)
 
+const heading = React.createElement("h1", { id: "heading" }, "hello sumit");
 
-const heading = React.createElement("h1",{id:"heading"},"hello sumit");
+console.log(heading);
+
+// JSX - HTML-like  or XML-like syntex
+
+const jsxHeading = <h1 id="heading">Namaste react using jsx</h1>;
+
+console.log(jsxHeading);
 
 const roots = ReactDOM.createRoot(document.getElementById("root"));
 
