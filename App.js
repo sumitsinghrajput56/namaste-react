b4c554e1946663a8dfe11a765624da6bbd888c4e
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2025-11-26 01:58:02 +0530

    3 ways to use functional component inside headingcomponent

--- App.js
+++ App.js
@@ -1,48 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const elem = <span>Namaste React</span>
 
-const number = 1000;
-
-const HeadingComponent = () => (
-  <div id="container">
-    {number}
-    <h2>{number}</h2>
-    <h2>{100+200}</h2>
-    {elem}
-    <h1 className="heading1">Namaste react functiona component </h1>
-  </div>
-);
-
-// React Element
-const title = (
+// React Functiona component
+const Title = () => (
   <h1 className="heading" tabIndex="1">
-    {<HeadingComponent/>}
     Namaste react using jsx
   </h1>
 );
 
-// React Component
-// Class Based Component - OLD
-// Functioan Based Component - NEW
-
-// React Functional Component
-
-const Heading1 = () => <h1 className="heading">Namaste react</h1>;
-
-const Heading2 = () => {
-  return <h1 className="heading1">Namaste react</h1>;
-};
-
-const Heading3 = () => {
-  <h1 className="heading1">Namaste react</h1>;
-};
-
-
+const HeadingComponent = () => (
+  <div id="container">
+   {Title()}
+   <Title/>
+   <Title></Title>
+    <h1 className="heading1">Namaste react functiona component </h1>
+  </div>
+);
 
 
 const roots = ReactDOM.createRoot(document.getElementById("root"));
 
 // How to render functional component
-roots.render(title);
+roots.render(<HeadingComponent/>);
