a985968cc536dd277028659d334cbafe86a2f43a
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2025-11-26 01:08:57 +0530

    functional component with capital letter starts name and like we use

--- App.js
+++ App.js
@@ -1,26 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// react element
+const heading = (
+  <h1 className="heading" tabIndex="1">
+    Namaste react using jsx
+  </h1>
+);
 
-// react element 
-const heading = (<h1 className="heading" tabIndex="1">Namaste react using jsx</h1>);
-
-// React Component 
+// React Component
 // Class Based Component - OLD
 // Functioan Based Component - NEW
 const roots = ReactDOM.createRoot(document.getElementById("root"));
 
-// React Functional Component 
+// React Functional Component
 
-const heading1 = () => <h1 className="heading">Namaste react</h1>;
+const Heading1 = () => <h1 className="heading">Namaste react</h1>;
 
-const heading2 = () => {
-return <h1 className="heading1">Namaste react</h1>
+const Heading2 = () => {
+  return <h1 className="heading1">Namaste react</h1>;
 };
 
-const heading3 = () => {
- <h1 className="heading1">Namaste react</h1>
+const Heading3 = () => {
+  <h1 className="heading1">Namaste react</h1>;
 };
 
+const Heading4 = () => {
+  <div id="container">
+    <h1 className="heading1">Namaste react</h1>
+  </div>;
+};
 
 roots.render(heading);
