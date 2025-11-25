3e2c669579b74a9e81ba3206963ca70cb55519c8
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2025-11-26 04:12:46 +0530

    raed about props

--- App.js
+++ App.js
@@ -37,12 +37,26 @@ const Header = () => {
   );
 };
 
-const RestaurantCard = () => {
-  return (<div className="res-card" style={{
-    backgroundColor: "#f0f0f0",
-}}>
-    <h3>Meghana Foods</h3>
-  </div>);
+const RestaurantCard = (props) => {
+    const {resName,cuisine} = props;
+  return (
+    <div
+      className="res-card"
+      style={{
+        backgroundColor: "#f0f0f0",
+      }}
+    >
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://www.eggoz.com/cdn/shop/articles/WhatsApp_Image_2024-09-07_at_5.05.44_PM.jpg?v=1725713796&width=1100"
+      />
+      <h3>{resName}</h3>
+      <h4>{cuisine}</h4>
+      <h4>4.4 Stars</h4>
+      <h4> 38 mins</h4>
+    </div>
+  );
 };
 
 const Body = () => {
@@ -50,7 +64,8 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurantCard />
+        <RestaurantCard resName="Meghna Biryani" cuisine="Biryani, North Indian, Asian"/>
+        <RestaurantCard resName="KFC" cuisine="Burger, Fast Food"/>
       </div>
     </div>
   );
