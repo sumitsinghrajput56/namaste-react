73790193086c123447f6883831dcecbd6e053673
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2025-11-25 20:34:44 +0530

    write code to change start or build in package.json and react and reactDOM

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
 
+// changing Object -> HTMLElemet using render method 
 
 
+const heading = React.createElement("h1",{id:"heading"},"hello sumit");
 
-const heading = React.createElement("div", {id: "parent"},[React.createElement("div", {id: "child"},[
-    React.createElement("h1", {id: "heading"},"first"),
-    React.createElement("h1", {id: "heading"},"second"),
-]),React.createElement("div", {id: "child1"},[
-    React.createElement("h1", {id: "heading"},"thired"),
-    React.createElement("h1", {id: "heading"},"forth"),
-])]);
+const roots = ReactDOM.createRoot(document.getElementById("root"));
 
-console.log(heading);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(heading);
+roots.render(heading);
