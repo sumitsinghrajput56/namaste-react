4b7d97ac2241b3c43d3a8b85a26042047d41117f
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2025-11-26 01:38:16 +0530

    react element inside react element using in functional component

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const elem = <span>Namaste React</span>
+
 // React Element
 const title = (
   <h1 className="heading" tabIndex="1">
+    {elem}
     Namaste react using jsx
   </h1>
 );
@@ -31,6 +34,7 @@ const HeadingComponent = () => (
     {number}
     <h2>{number}</h2>
     <h2>{100+200}</h2>
+    {title}
     <h1 className="heading1">Namaste react functiona component </h1>
   </div>
 );
