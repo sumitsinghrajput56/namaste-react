1accb75eea429efdc2803d8105d64e60bfd44609
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2025-11-26 01:28:47 +0530

    How cani use javascript number variable

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// functional component
-const Title = function () {
-  return (
-    <h1 className="heading" tabIndex="1">
-      Namaste react using jsx
-    </h1>
-  );
-};
+// React Element
+const title = (
+  <h1 className="heading" tabIndex="1">
+    Namaste react using jsx
+  </h1>
+);
 
 // React Component
 // Class Based Component - OLD
@@ -26,9 +24,11 @@ const Heading3 = () => {
   <h1 className="heading1">Namaste react</h1>;
 };
 
-const Heading4 = () => (
+const number = 1000;
+
+const HeadingComponent = () => (
   <div id="container">
-    <Title />
+    {number}
     <h1 className="heading1">Namaste react functiona component </h1>
   </div>
 );
@@ -36,4 +36,4 @@ const Heading4 = () => (
 const roots = ReactDOM.createRoot(document.getElementById("root"));
 
 // How to render functional component
-roots.render(<Heading4 />);
+roots.render(<HeadingComponent />);
