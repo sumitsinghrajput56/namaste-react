186dbf7bcab8a96b5baa51793a93451340ab91d1
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2025-11-26 01:05:37 +0530

    functional component with () {} and single line

--- App.js
+++ App.js
@@ -5,9 +5,22 @@ import ReactDOM from "react-dom/client";
 // react element 
 const heading = (<h1 className="heading" tabIndex="1">Namaste react using jsx</h1>);
 
-console.log(heading);
-
-
+// React Component 
+// Class Based Component - OLD
+// Functioan Based Component - NEW
 const roots = ReactDOM.createRoot(document.getElementById("root"));
+
+// React Functional Component 
+
+const heading1 = () => <h1 className="heading">Namaste react</h1>;
+
+const heading2 = () => {
+return <h1 className="heading1">Namaste react</h1>
+};
+
+const heading3 = () => {
+ <h1 className="heading1">Namaste react</h1>
+};
+
 
 roots.render(heading);
