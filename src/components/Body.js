aa6546fb4a74a714eabdb70bdb0f68eb96ff6426
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2025-11-30 12:30:16 +0530

    Implemented search functionality Here

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,28 +1,63 @@
 import { useEffect, useState } from "react";
 import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-    // Local State Variable - Super powerful variable 
+  // Local State Variable - Super powerful variable
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+
+  // whenever state variable update, react triggers a reconciliation cycle (re-renders the component) 
+
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
-      fetchData();
+    fetchData();
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9804517&lng=77.746281&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9804517&lng=77.746281&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
 
     const json = await data.json();
 
-    // Optional Chaning 
+    // Optional Chaning
     // Install Cros Extensions
-    setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-  }
+    setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+      setFilteredRestaurant(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
 
-  return (
+  // Conditional Rendering
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            placeholder="Search Restaurants"
+            value={searchText}
+            onChange={(e)=>{setSearchText(e.target.value)}}
+          />
+          <button
+            onClick={() => {
+              const filteredRestaurant = listOfRestaurants.filter((res) =>
+                (res.info.name.toLowerCase()).includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
@@ -36,7 +71,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurent) => (
+        {filteredRestaurant.map((restaurent) => (
           <RestaurantCard key={restaurent.info.id} resData={restaurent} />
         ))}
         {/* <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" /> */}
